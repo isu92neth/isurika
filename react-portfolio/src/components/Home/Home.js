8a522312297478dd3f4a48d09520bc3ade45a597
isu92neth <123csissa@gmail.com>
2021-06-20 18:54:31 +0530

    Styled Home

--- react-portfolio/src/components/Home/Home.js
+++ react-portfolio/src/components/Home/Home.js
@@ -1,7 +1,14 @@
+import classes from "./Home.module.css";
+import homeImg from "./../../img/home.jpg";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={classes.Home}>
+      <div className={classes.Container}>
+        <h1 className={classes.Hello}>Hello World</h1>
+        <h1>Welcome on my website</h1>
+      </div>
+      <img className={classes.HomeImg} src={homeImg} alt="homeImage"></img>
     </div>
   );
 };
