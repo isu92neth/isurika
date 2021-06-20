ddc34e7757e7f4d93addd8b190a8b0e35de9e698
isu92neth <123csissa@gmail.com>
2021-06-20 16:49:34 +0530

    Injected components

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div className="App"></div>;
-}
+import NavBar from "./components/NavBar/NavBar";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Home from "./components/Home/Home";
+import Projects from "./components/Projects/Projects";
+import Skills from "./components/Skills/Skills";
+
+const App = () => {
+  return (
+    <div className="App">
+      <NavBar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </div>
+  );
+};
 
 export default App;
