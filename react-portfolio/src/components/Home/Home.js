6ce8c363bda38c0b14b89ae0ab3b671409d72a67
isu92neth <123csissa@gmail.com>
2021-06-22 13:49:08 +0530

    Modified Home

--- react-portfolio/src/components/Home/Home.js
+++ react-portfolio/src/components/Home/Home.js
@@ -5,10 +5,12 @@ const Home = () => {
   return (
     <div className={classes.Home}>
       <div className={classes.Container}>
-        <h1 className={classes.Hello}>Hello World</h1>
-        <h1>Welcome on my website</h1>
+        <h1 className={classes.Name}>Isurika Adikari</h1>
+        <h1 className={classes.SubTopic}>
+          Computer Engineering, University of Peradeniya, Sri Lanka
+        </h1>
       </div>
-      <img className={classes.HomeImg} src={homeImg} alt="homeImage"></img>
+      {/*<img className={classes.HomeImg} src={homeImg} alt="homeImage"></img>*/}
     </div>
   );
 };
